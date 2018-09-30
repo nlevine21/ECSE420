c9042171baf704505e5bfa99a60ed95a4ebcd99c
Jordan C. Itzkovitz <jcitzko@hotmail.com>
2018-09-30 16:51:38 -0400

    Changed solutions for Q3 p2 and p3 - added comments and explanations for each situation (deadlock, starvation, neither)

--- src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
+++ src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
@@ -3,49 +3,54 @@ package ca.mcgill.ecse420.a1;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class DiningPhilosophers {
-	
+	private static Philosopher[] philosophers;
 	private static Semaphore[] chopsticks;
-	private static Semaphore waiter = new Semaphore(1);
-	private static Semaphore stoppedEating = new Semaphore(1);
-	private static int eaters = 0;
-	
+	private static Semaphore mutex = new Semaphore(1);
+	private static int situation;
+	private static final int NUM_PHILOSOPHERS = 5;
 	
 	public static void main(String[] args) {
 
 		// Initialize variables
-		int numberOfPhilosophers = 5;
-		ExecutorService executor = Executors.newFixedThreadPool(numberOfPhilosophers);
-        Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
-        chopsticks = new Semaphore[numberOfPhilosophers];
+		ExecutorService executor = Executors.newFixedThreadPool(NUM_PHILOSOPHERS);
+        philosophers = new Philosopher[NUM_PHILOSOPHERS];
+        chopsticks = new Semaphore[NUM_PHILOSOPHERS];
         
-        // Decide whether you would like to see the situation in deadlock (0), starving (1), or neither (2)
-        int situation = 2;
+        // Decide whether you would like to see the situation in deadlock (0), where some threads may starve (1), or neither (2)
+        situation = 2;
         
-        for(int i = 0; i < numberOfPhilosophers; i++){
-        	philosophers[i] = new Philosopher(i,numberOfPhilosophers, situation);
+        for(int i = 0; i < NUM_PHILOSOPHERS; i++){
+        	philosophers[i] = new Philosopher(i);
         	chopsticks[i] = new	Semaphore(1);
         }
         
-        for(int i = 0; i < numberOfPhilosophers; i++){
+        for(int i = 0; i < NUM_PHILOSOPHERS; i++){
         	executor.execute(philosophers[i]);
         }
 
 	}
-
+	
+	/**
+	 * Class that defines the Philosopher thread
+	 *
+	 */
 	public static class Philosopher implements Runnable {
 		
 		int philosopherIndex;
-	    int totalNumPhilosophers;
-		int situation;
 		String state;
 		
-		public Philosopher(int philosipherIndex, int totalNumPhilosiphers, int situation){
+		/**
+		 * Philosopher constructor function
+		 * 
+		 * @param philosipherIndex is the index of the philosopher in the philosophers array
+		 * @param totalNumPhilosiphers is the total number of philosophers
+		 * @param situation is the situation in which this philosopher will run under/determines which solution will be used
+		 */
+		public Philosopher(int philosipherIndex){
 			this.philosopherIndex = philosipherIndex;
-			this.totalNumPhilosophers = totalNumPhilosiphers;
-			this.situation = situation;
+			this.state = "HUNGRY";
 		}
 
 		@Override
@@ -53,10 +58,15 @@ public class DiningPhilosophers {
 			
 			while(true){
 				try{
-					// Situation which causes deadlock - each philosopher goes directly for their chopstick and then the chopstick to their right
+					/* Situation 0 is that of which ends in deadlock. Each philosopher picks up their chopstick first and then goes for the chopstick to their right (or next index).
+					 * However, they will not be able to obtain the adjacent chopstick as their neighbour will hold it, and thus will wait until it becomes available. Because
+					 * there are not rules to manage this situation, each Philosopher will wait on another to give up their chopstick, which ultimately will never happen. The
+					 * eat, drop, and think methods are written out despite knowing that the code will never reach those steps to demonstrate that the program is in deadlock -
+					 * each of the aforementioned methods have print statements, which will never be seen.
+					 * */
 					if(situation == 0){
 						// Philosopher attempts to take the closest chopstick to them
-						pickupChopstick(philosopherIndex,philosopherIndex);
+						pickupChopstick(philosopherIndex);
 						
 						//Simulate the fact that each philosopher goes for their chopsticks at the same exact time
 						//With no sleep, some thread could obtain the lock of the next chopstick before that respective thread can run
@@ -66,142 +76,170 @@ public class DiningPhilosophers {
 						}
 										
 						// Philosopher attempts to take the next closest chopstick
-						pickupChopstick(philosopherIndex,(philosopherIndex+1)%totalNumPhilosophers);
+						pickupChopstick(rightNeighbor());
 						
 						// Philosopher eats
-						eat(philosopherIndex);
-						
-						// Philosopher puts down their chopsticks
-						dropChopsticks(philosopherIndex);
-						
-						// Philosopher thinks
-						think(philosopherIndex);
-					}
-					
-					// Situation which causes starvation, but no deadlock - let the last philosopher think before picking up their chopstick
-					else if(situation == 1){
-						if(philosopherIndex == totalNumPhilosophers-1){
-							//while(availableChopstick(philosopherIndex) || availableChopstick((philosopherIndex+1)%totalNumPhilosophers)){}
-						}
-						
-						// Philosopher picks up chopsticks
-						pickupChopsticks(philosopherIndex);
-						
-						// Philosopher eats
-						eat(philosopherIndex);
+						eat();
 						
 						// Philosopher drops chopsticks
-						dropChopsticks(philosopherIndex);
+						dropChopstick(philosopherIndex);
+						dropChopstick(rightNeighbor());
 						
 						// Philosopher thinks
-						think(philosopherIndex);
-						
+						think();
 					}
-					
-					
-					// Situation which causes neither deadlock nor starvation
+					/* Situtation 2 is that of which will never end in deadlock, however has potential to cause starvation in some threads at some point. The Philosopher picks
+					 * up their chopstick and then reaches for the chopstick to their right (or next index). They will continuously try to pickup the next chopstick until they
+					 * have waited a random period of time, in which case they will drop their chopstick, wait another random period of time, and go through the same process
+					 * again. This will thus give the chance of another Philosopher to pickup the chopstick that they need to eat in order for them to drop their own chopstick and
+					 * allow for other Philosophers to eat as well (and so on). This solution will never end in deadlock as there are always enough chopsticks available to be
+					 * effectively used and passed on to another Philosopher who has been waiting for it. However, the solution might cause starvation if a specific
+					 * Philosopher is "unlucky" and the chopsticks they need are constantly in use and being swapped by other Philosophers while that Philosopher is waiting to begin
+					 * the process again. This can technically happen for a long period of time and cause that Philosopher to starve.
+					 * */
+					else if(situation == 1){
+						// Philosopher attempts to pickup both chopsticks
+						while(true){
+							// Philosopher attempts to take the closes chopstick to them
+							pickupChopstick(this.philosopherIndex);
+							
+							// Philosopher attempts to pickup the next closest chopstick,
+							// however if they cannot after a random period of time they drop the chopstick they currently have
+							long startTime;
+							boolean droppedChopstick = false;
+							startTime = System.currentTimeMillis();
+							while(!chopsticks[rightNeighbor()].tryAcquire()){
+								
+								// If philosopher has held their chopstick for too long without acquiring the next, drop it
+								if(System.currentTimeMillis() - startTime > Math.random()*10000){
+									
+									// Philosopher drops the chopstick they currently hold
+									dropChopstick(this.philosopherIndex);
+									System.out.println("PHILOSOPHER " + this.philosopherIndex + " could not get the next chopstick, so dropped their own");
+									
+									// Philosopher waits, and then will try to acquire their chopstick again
+									Thread.sleep((long) (Math.random()*10000));
+									droppedChopstick = true;
+									break;
+								}
+							}
+							
+							// If the chopstick was not dropped and the acquire attempt was successful, begin eating
+							if(!droppedChopstick) {
+								System.out.println("PHILOSOPHER "+this.philosopherIndex+" gets chopstick "+rightNeighbor());
+								break;
+							}
+						}
+						
+						// Philosopher eats
+						eat();
+						
+						// Philosopher drops chopsticks
+						dropChopstick(philosopherIndex);
+						dropChopstick(rightNeighbor());
+						
+						// Philosopher thinks
+						think();
+						
+					} 
+					/* Situation 2 is that of which causes no deadlock nor starvation. This solution forces the philosophers to be aware of the state of their neighbors 
+					 * (HUNGRY, EATING, THINKING) and to act accordingly. A Philosopher will only take both chopsticks if they are both available, ie. either neighbor is not 
+					 * eating and thus are not utilizing the chopsticks they will need. While a Philosopher does not have either chopstick, they wait until it is their turn 
+					 * to acquire them. Once they eat and drop their chopsticks, the next Philosopher in line will have access to the chopsticks they have been waiting for. 
+					 * There is no direct implementation of a queue in this solution, however the use of semaphores simulates similar functionality in that the system knows 
+					 * when the resource was requested and by which Philosopher, and they will pickup their chopsticks when it is their turn. Thus, all Philosophers will have 
+					 * their turn to eat without the potential of starving as there is no random variable thats causing them to miss their turn (unlike the previous solution).
+					 * */
 					else if(situation == 2){
+						// While the philosopher is hungry, continuously try to pickup chopsticks
+						this.state = "HUNGRY";
+						System.out.println("PHILOSOPHER "+this.philosopherIndex+" IS HUNGRY");
+						while(this.state.equals("HUNGRY")){
+							mutex.acquire();
+							
+							// If the Philosopher is still hungry and both their left and right neighbours are not eating, pickup both chopsticks
+							if(this.state.equals("HUNGRY") 
+									&& !philosophers[leftNeighbor()].state.equals("EATING")
+									&& !philosophers[rightNeighbor()].state.equals("EATING")){
+								this.state = "EATING";
+								pickupChopstick(this.philosopherIndex);
+								pickupChopstick(rightNeighbor());
+							}
+							mutex.release();
+						}
 						
-					// Philosopher waits for waiters permission to take chopsticks
-					System.out.println("PHILOSOPHER " + philosopherIndex + " IS HUNGRY");
-					getWaiterPermission();
-					
-					// Philosopher takes chopsticks nearest to them
-					pickupChopsticks(philosopherIndex);
-					
-					// Philosopher eats
-					eat(philosopherIndex);
-					
-					// Philosopher drops chopsticks
-					dropChopsticks(philosopherIndex);
-					
-					// Philosopher notifies waiter that they have dropped their chopsticks
-					informWaiter();
-					
-					// Philosopher thinks
-					think(philosopherIndex);
-					
+						// Philosopher eats
+						eat();
+						
+						// Philosopher drops chopsticks
+						mutex.acquire();
+						this.state = "THINKING";
+						dropChopstick(this.philosopherIndex);
+						dropChopstick(rightNeighbor());
+						mutex.release();
+						
+						// Philosopher thinks
+						think();
 					}
-				} catch (InterruptedException e){}
-				
+				} catch (InterruptedException e){}	
 			}
 		}
 		
-		private void getWaiterPermission() throws InterruptedException{
-			synchronized(waiter){
-				synchronized(stoppedEating){
-					// Wait to get waiters attention
-					waiter.acquire();
-					
-					// Increment the total number of eaters
-					eaters++;
-					
-					// If there are too many eaters, make them wait until another philosopher stops eating
-					// Otherwise, let the waiter continue to delegate
-					if(eaters > totalNumPhilosophers-1){
-						waiter.release();
-						stoppedEating.acquire();
-					}else
-						waiter.release();			
-				}
-			}
-		}
-		
-		private void informWaiter() throws InterruptedException{
-			synchronized(waiter){
-				synchronized(stoppedEating){
-					// Wait for waiters attention
-					waiter.acquire();
-					
-					// Notify that there is one less eater
-					eaters--;
-					
-					// If the number of eaters previously exceeded the allowed amount and caused a philosopher to wait for them to stop, notify that they have stopped
-					if(eaters == totalNumPhilosophers-1){
-						stoppedEating.release();
-					}
-					
-					// Let the waiter continue to delegate
-					waiter.release();
-				}
-			}
-		}
-		
-		
-		private void pickupChopstick(int philosopher, int chopstick) throws InterruptedException{
+		/**
+		 * Void method that (eventually) acquires a chopstick and prints a statement
+		 * 
+		 * @param chopstick is the index of the chopstick that is being requested for acquisition
+		 * */
+		private void pickupChopstick(int chopstick) throws InterruptedException{
 			synchronized(chopsticks){
 				chopsticks[chopstick].acquire();
-				System.out.println("PHILOSOPHER "+philosopher+" gets chopstick "+chopstick);
+				System.out.println("PHILOSOPHER "+this.philosopherIndex+" gets chopstick "+chopstick);
 			}
 		}
 		
-		private void pickupChopsticks(int philosopher) throws InterruptedException{
-			synchronized(chopsticks){
-				chopsticks[philosopher].acquire();
-				System.out.println("PHILOSOPHER "+philosopher+" gets chopstick "+philosopherIndex);
-				chopsticks[(philosopherIndex+1)%totalNumPhilosophers].acquire();
-				System.out.println("PHILOSOPHER "+philosopher+" gets chopstick "+(philosopherIndex+1)%totalNumPhilosophers);
-			}
+		/**
+		 * Void method that releases a chopstick and prints a statement
+		 * 
+		 * @param chopstick is the index of the chopstick that is being requested for release
+		 * */
+		private void dropChopstick(int chopstick) throws InterruptedException{
+			System.out.println("PHILOSOPHER "+this.philosopherIndex+" drops chopstick "+chopstick);
+			chopsticks[chopstick].release();
 		}
 		
-		private void dropChopsticks(int philosopher){
-				chopsticks[philosopher].release();
-				chopsticks[(philosopherIndex+1)%totalNumPhilosophers].release();
-				System.out.println("PHILOSOPHER "+philosopher+" puts down chopstick "+philosopher+" and chopstick "+(philosopherIndex+1)%totalNumPhilosophers);
-		}
-		
-		private void think(int philosopher){
-			System.out.println("PHILOSOPHER " + philosopherIndex + " IS THINKING");
+		/**
+		 * Void method that prints a statement telling the user that the philosopher is thinking and puts the thread to sleep
+		 * 
+		 * */
+		private void think(){
+			System.out.println("PHILOSOPHER " + this.philosopherIndex + " IS THINKING");
 			try {
-				Thread.sleep(5000);
+				Thread.sleep(3000);
 			} catch (InterruptedException e) {}
 		}
 		
-		private void eat(int philosopher){
-			System.out.println("PHILOSOPHER " + philosopherIndex + " IS EATING");
+		/**
+		 * Void method that prints a statement telling the user that the philosopher is eating and puts the thread to sleep
+		 * 
+		 * */
+		private void eat(){
+			System.out.println("PHILOSOPHER " + this.philosopherIndex + " IS EATING");
 			try {
-				Thread.sleep(5000);
+				Thread.sleep(3000);
 			} catch (InterruptedException e) {}
+		}
+		
+		/**
+		 * Returns the index of the "left" neighbor to the given philosopher
+		 * 
+		 * */
+		private int leftNeighbor(){
+			if(this.philosopherIndex-1 < 0) return NUM_PHILOSOPHERS-1;
+			return this.philosopherIndex-1;
+		}
+		
+		private int rightNeighbor(){
+			return (this.philosopherIndex+1)%NUM_PHILOSOPHERS;
 		}
 
 	}
