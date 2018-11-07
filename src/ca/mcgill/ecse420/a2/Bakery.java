9e9d9a8a2e6634559a6279821fba56ef2ebdb447
Noah Levine <noahlevine@Noahs-MacBook-Air.local>
2018-11-07 18:20:29 -0500

    assignment 2 code

--- src/ca/mcgill/ecse420/a2/Bakery.java
+++ src/ca/mcgill/ecse420/a2/Bakery.java
@@ -5,43 +5,44 @@ public class Bakery {
 	long[] label;
 	int numThreads;
 	
-	public Bakery(int n){
+	public Bakery(int n) {
 		flag = new boolean[n];
 		label = new long[n];
 		numThreads = n;
+
 		for(int i = 0; i < n; i++){
 			flag[i] = false;
 			label[i] = 0;
 		}
 	}
 	
-	public void lock(int threadId){
+	public void lock(int threadId) {
 		flag[threadId] = true;
 		long max = 0;
-		for(int id = 0; id < numThreads; id++){
-			if(label[id] > max)
+
+		for(int id = 0; id < numThreads; id++) {
+			if(label[id] > max) {
 				max = label[id];
+			}
 		}
 		
 		label[threadId] = max+1;
-		for(int id = 0; id < numThreads; id++){
-			while(id != threadId && flag[id] && lexicoCompare(threadId,id)){
-			}
+		for(int id = 0; id < numThreads; id++) {
+			while(id != threadId && flag[id] && lexicoCompare(threadId,id));
 		}
 	}
 	
-	public void unlock(int threadId){
+	public void unlock(int threadId) {
 		flag[threadId] = false;
 	}
 	
-	private boolean lexicoCompare(int id1, int id2){
+	private boolean lexicoCompare(int id1, int id2) {
 		if(label[id1] > label[id2]) {
 			return true;
 		}
-		else if(label[id1] == label[id2]){
+		else if(label[id1] == label[id2]) {
 			return id1 > id2;
 		}
 		return false;
 	}
-	
 }
--- src/ca/mcgill/ecse420/a2/LockTest.java
+++ src/ca/mcgill/ecse420/a2/LockTest.java
@@ -1,4 +1,5 @@
 package ca.mcgill.ecse420.a2;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -6,15 +7,16 @@ public class LockTest {
 	static int accountBalance = 0;
 	static int numThreads = 40;
 	
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		
 		// First show a bank account being accessed by multiple threads
 		// with no locking mechanism
 		ExecutorService executorNoLock = Executors.newFixedThreadPool(numThreads);
-		for(int i = 0; i < numThreads; i++)
+		for(int i = 0; i < numThreads; i++) {
 			executorNoLock.execute(new UnsafeDeposit(i));
+		}
 		executorNoLock.shutdown();
-		while(!executorNoLock.isTerminated()){}
+		while(!executorNoLock.isTerminated());
 		System.out.println("Account balance without a locking mechanism: "+accountBalance);
 		
 		// Reset the account balance
@@ -24,10 +26,11 @@ public class LockTest {
 		// with the Filter locking mechanism
 		ExecutorService executorFilter = Executors.newFixedThreadPool(numThreads);
 		Filter filter = new Filter(numThreads);
-		for(int i = 0; i < numThreads; i++)
-			executorFilter.execute(new FilterSafeDeposit(i,filter));
+		for(int i = 0; i < numThreads; i++) {
+			executorFilter.execute(new FilterSafeDeposit(i, filter));
+		}
 		executorFilter.shutdown();
-		while(!executorFilter.isTerminated()){}
+		while(!executorFilter.isTerminated());
 		System.out.println("Account balance using the Filter locking mechanism: "+accountBalance);
 		
 		// Reset the accountBalance
@@ -37,21 +40,22 @@ public class LockTest {
 		// with the Bakery locking mechanism
 		ExecutorService executorBakery = Executors.newFixedThreadPool(numThreads);
 		Bakery bakery = new Bakery(numThreads);
-		for(int i = 0; i < numThreads; i++)
-			executorBakery.execute(new BakerySafeDeposit(i,bakery));
+		for(int i = 0; i < numThreads; i++) {
+			executorBakery.execute(new BakerySafeDeposit(i, bakery));
+		}
 		executorBakery.shutdown();
-		while(!executorBakery.isTerminated()){}
+		while(!executorBakery.isTerminated());
 		System.out.println("Account balance using the Bakery locking mechanism: "+accountBalance);
 	}
 	
-	public static class UnsafeDeposit implements Runnable{
+	public static class UnsafeDeposit implements Runnable {
 		int id;
 		
 		public UnsafeDeposit(int id){
 			this.id = id;
 		}
 		
-		public void run(){
+		public void run() {
 			
 			try {
 				Thread.sleep(200);
@@ -64,15 +68,16 @@ public class LockTest {
 		}
 	}
 	
-	public static class FilterSafeDeposit implements Runnable{
+	public static class FilterSafeDeposit implements Runnable {
 		int id;
 		Filter filter;
-		public FilterSafeDeposit(int id, Filter filter){
+
+		public FilterSafeDeposit(int id, Filter filter) {
 			this.id = id;
 			this.filter = filter;
 		}
 		
-		public void run(){
+		public void run() {
 			try {
 				Thread.sleep(200);
 				filter.lock(id);
@@ -84,15 +89,16 @@ public class LockTest {
 		}
 	}
 	
-	public static class BakerySafeDeposit implements Runnable{
+	public static class BakerySafeDeposit implements Runnable {
 		int id;
 		Bakery bakery;
-		public BakerySafeDeposit(int id, Bakery bakery){
+
+		public BakerySafeDeposit(int id, Bakery bakery) {
 			this.id = id;
 			this.bakery = bakery;
 		}
 		
-		public void run(){
+		public void run() {
 			try {
 				Thread.sleep(200);
 				bakery.lock(id);
